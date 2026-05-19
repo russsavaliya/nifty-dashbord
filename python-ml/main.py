80994f1a4d725e28a42420484583005241ac0302
vivek <vivek.t@buzzinga.design>
2026-05-19 14:20:33 +0530

    setup for pythone deployement

--- python-ml/main.py
+++ python-ml/main.py
@@ -13,9 +13,14 @@ from ml_model import NiftyPredictor, METRICS_PATH
 
 app = FastAPI(title="NiftyPredictor ML Service", version="3.0.0")
 
+_origins = os.getenv(
+    "ALLOWED_ORIGINS",
+    "http://localhost:3000,http://127.0.0.1:3000"
+).split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
